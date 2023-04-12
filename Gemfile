67c30fdcebf2ceda8706aac042ca311b30cfc35c
Anastasia Burchalova <anastasia.burchalova@aterise.com>
2023-04-12 03:11:14 +0300

    Cleanup

--- Gemfile
+++ Gemfile
@@ -9,10 +9,6 @@ gem 'mysql2', '~> 0.5'
 gem 'puma', '~> 5.0'
 gem 'rails', '~> 7.0.4', '>= 7.0.4.3'
 gem 'sprockets-rails'
-# gem 'importmap-rails'
-# gem 'turbo-rails'
-# gem 'stimulus-rails'
-# gem 'jbuilder'
 gem 'bootsnap', require: false
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
@@ -20,7 +16,6 @@ gem 'faker'
 gem 'graphql'
 
 group :development, :test do
-  # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
 
   gem 'pry'
--- app/graphql/graphql_challenge_schema.rb
+++ app/graphql/graphql_challenge_schema.rb
@@ -16,29 +16,6 @@ class GraphqlChallengeSchema < GraphQL::Schema
   max_complexity MAX_COMPLEXITY
   max_depth MAX_DEPTH
 
-  # GraphQL-Ruby calls this when something goes wrong while running a query:
-
-  # Union and Interface Resolution
-  def self.resolve_type(_abstract_type, _obj, _ctx)
-    # TODO: Implement this method
-    # to return the correct GraphQL object type for `obj`
-    raise(GraphQL::RequiredImplementationMissingError)
-  end
-
   # Stop validating when it encounters this many errors:
   validate_max_errors(MAX_ERRORS)
-
-  # Relay-style Object Identification:
-
-  # Return a string UUID for `object`
-  def self.id_from_object(object, _type_definition, _query_ctx)
-    # For example, use Rails' GlobalID library (https://github.com/rails/globalid):
-    object.to_gid_param
-  end
-
-  # Given a string UUID, find the object
-  def self.object_from_id(global_id, _query_ctx)
-    # For example, use Rails' GlobalID library (https://github.com/rails/globalid):
-    GlobalID.find(global_id)
-  end
 end
